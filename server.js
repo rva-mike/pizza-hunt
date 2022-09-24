df9778cbed3ce6e8f92fa6157e8559d42351f988
rva-mike <mike.levy.rva@gmail.com>
2022-09-24 16:24:12 -0400

    deploying

--- server.js
+++ server.js
@@ -11,10 +11,18 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/main-db',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
